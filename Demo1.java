6cd1d60ad923a9ac618094219bcb2af2c35799b7
fpl890 <47425978+fpl890@users.noreply.github.com>
2019-02-27 20:49:06 -0700

    Add files via upload

--- Demo1.java
+++ Demo1.java
@@ -4,37 +4,37 @@ import java.util.Random;
 
 public class Demo1{
     /**
-
+     * First Class Demo - Firoz, Matthew, Sarina, Doug, Alexis
      */
     public static void main(String args[]){
-        Random random = new Random();
+        Random random = new Random(); //used to pick a random object from the list
         Board gameBoard = new Board();
-        gameBoard.populateBoard();
-        Player player =  new Player("John");
-       /* ArrayList<Square> squares = gameBoard.RandomizeList(gameBoard.getList());
-        for (Square square : squares){
-            System.out.println(square.getTitle());
-
-        }*/
+        gameBoard.populateBoard(); // populates the board with a bunch of squares
+        Player player =  new Player("John"); // used to keep track of the players status in the game
+       
         Scanner keyboard  = new Scanner(System.in);
+
+        //loop will run forever unless the user presses the W key
         while(true){
-            ArrayList<Square> squares = gameBoard.RandomizeList(gameBoard.getList());
+
+            ArrayList<Square> squares = gameBoard.RandomizeList(gameBoard.getList()); //returns a random arraylist from the gameboard
+            
+            //prints all elements of the random list
             for (Square square : squares){
                 System.out.println(square.getTitle());
-    
             }
-            System.out.println("Press Q to spin and W to exit");
+            //user prompt 
+            System.out.println("Press any key to spin and W to exit");
             String input = keyboard.nextLine();
-            if(input.equalsIgnoreCase("q")){
-                Square newAward = squares.get(random.nextInt(squares.size()));
-                player.addAward(newAward);
-                System.out.println("\nYour reward: " +newAward.getTitle()+"\n\n\n");
 
-                for (Square square : squares){
-                    System.out.println(square.getTitle());
-                }  
-        
-            } 
+           //adds the award to the players list of awards
+            Square newAward = squares.get(random.nextInt(squares.size()));
+            player.addAward(newAward);
+            
+            System.out.println("\nYour reward: " +newAward.getTitle()+"\n\n\n");
+
+                
+            
             
             if(input.equalsIgnoreCase("w")){
                 break;
@@ -42,7 +42,10 @@ public class Demo1{
 
            
         }
-        System.out.println("Your awards: ");
+        
+        //prints the player's awards to the screen
+        
+        System.out.println("\n\n\nYour awards: ");
         for (Square square : player.getAwards()){
             System.out.println(square.getTitle());
         }  
