0adcf2de4b40501eaa3e6bf91f1add97ce6a624b
mforman000 <47426001+mforman000@users.noreply.github.com>
2019-03-12 13:27:19 -0600

    Add files via upload

--- Player.java
+++ Player.java
@@ -1,20 +1,12 @@
 import java.util.ArrayList;
-/*
-* The Player class keeps track of the player's status throughout the game
-* This class can modify and access the player name, number of spins, score and the number of strikes
-*/
+
 public class Player{
     private String name;
     private int numOfSpins;
     private int score;
     private int strikes;
     private ArrayList<Square> awards = new ArrayList<Square>();
-    /* setter constructor that allows you to set all the instance variables in the new object
-    * @param setName gets assigned to the player name
-    * @param setSpins gets assigned to the number of spins 
-    * @param setScore gets assigned to the player score
-    * @param setStrikes gets assigned to the number of strikes
-    */
+    //setter constrcutor that allows you to set all the iunstacne variables in the new object
     public Player(String setName, int setSpins, int setScore, int setStrikes){
         this.name = setName;
         this.numOfSpins = setSpins;
@@ -22,9 +14,7 @@ public class Player{
         this.strikes = setStrikes;
         this.awards = new ArrayList<Square>();
     }
-    /*Default constructor that allows you to set the name and gives the other instance variables their default values
-    * @param setName gets assigned to the player name
-    */
+    //Default constructor that allows you to set the name and gives the other instance variables their default values
     public Player(String setName){
         this.name = setName;
         this.numOfSpins = 0;
@@ -32,9 +22,7 @@ public class Player{
         this.strikes = 0;
         this.awards = new ArrayList<Square>();
     }
-    /*Copy Constructor
-    * @param p of type Player who's instance variables get copied and reassigned to this object
-    */
+    //Copy Constructor
     public Player(Player p){
         this.name = p.name;
         this.numOfSpins = p.numOfSpins;
@@ -42,83 +30,51 @@ public class Player{
         this.strikes = p.strikes;
         this.awards = p.awards;
     }
-    /*
-    * @return the name of the player
-    */
+
     public String getName(){
         Player getterPlayer = new Player(this.name, this.numOfSpins, this.score, this.strikes);
         return getterPlayer.name;
     }
-    /*
-    * @return the number of spins the player has accumulated
-    */
+
     public int getSpins(){
         Player getterPlayer = new Player(this.name, this.numOfSpins, this.score, this.strikes);
         return getterPlayer.numOfSpins;
     }
-    
-    /*
-    * @return the player score
-    */
+
     public int getScore(){
         Player getterPlayer = new Player(this.name, this.numOfSpins, this.score, this.strikes);
         return getterPlayer.score;
     }
     
-    /*
-    * @return the number of strikes
-    */
     public int getStrikes(){
         Player getterPlayer = new Player(this.name, this.numOfSpins, this.score, this.strikes);
         return getterPlayer.strikes;
     }
-    
-    /*
-    * @param newName is assigned to the player name
-    */
+
     public void setName(String newName){
         this.name = newName;
     }
-    
-    /*
-    * @param newSpins is assigned to the numOfSpins instance variable
-    */
-   
+
     public void setSpins(int newSpins){
         this.numOfSpins = newSpins;
     }
-    
-    /**
-    *@param addedSpins is added to the already existing number of spins
-    */
+
     public void addSpins(int addedSpins){
         this.numOfSpins += addedSpins;
     }
-    /*
-    * @param newScore is assigned to the score instance variable
-    */
-   
+    
     public void setScore(int newScore){
         this.score = newScore;
     }
-    
-    /*
-    * @param newStrikes is assigned to the strikes instance variable
-    */
-   
+
     public void setStrikes(int newStrikes){
         this.strikes = newStrikes;
     }
-    
-    /** 
-    * This adds @param newAward to the awards array in the player class
-    */
+
     public void addAward(Square newAward){
         this.awards.add(newAward);
     }
-    /** 
-    * this returns a copy of the ArrayList awards in order to maintain security of the awards list a player has
-    */
+
     public ArrayList<Square> getAwards(){
         ArrayList<Square> getterAwards = new ArrayList<Square>();
         for (Square a : this.awards){
