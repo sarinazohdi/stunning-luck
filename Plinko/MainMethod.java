ba71475fb609e6bf6fba8e8ba4540e9ee3f28c94
mforman000 <47426001+mforman000@users.noreply.github.com>
2019-03-13 15:06:44 -0600

    Update MainMethod.java

--- Plinko/MainMethod.java
+++ Plinko/MainMethod.java
@@ -1,6 +1,8 @@
 public class MainMethod{
     public static void main(String[] args){
+        Player player = new Player("john doe");
         String[] starter = new String[] {"1","2","3"};
         PlinkoGui.main(starter);
+        player.setSpins(PlinkoGui.getSpinsFromPlinko());
     }
 }
