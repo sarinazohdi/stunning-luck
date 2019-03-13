b2c61b8d8ada65fbf3c2bd8affca61e5803eaa43
mforman000 <47426001+mforman000@users.noreply.github.com>
2019-03-13 15:45:10 -0600

    Update MainMethod.java

--- Plinko/MainMethod.java
+++ Plinko/MainMethod.java
@@ -1,8 +1,43 @@
-public class MainMethod{
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.Pane;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+
+public class MainMethod extends Application{
+    private Player player1 = new Player("John Doe");
+
+    public void start(Stage primaryStage){
+
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 600, 600);
+
+        Button plinkoButton = new Button("Plinko");
+        plinkoButton.setLayoutX(50);
+        plinkoButton.setLayoutY(300);
+        plinkoButton.setOnAction(new EventHandler<ActionEvent>(){
+            public void handle(ActionEvent t){
+                Stage plinkoStage = new Stage();
+                new PlinkoGui().start(plinkoStage);
+                player1.setSpins(PlinkoGui.getSpinsFromPlinko());
+            }
+        });
+
+        root.getChildren().add(plinkoButton);
+
+
+
+        primaryStage.setTitle("Stunning Luck");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
     public static void main(String[] args){
-        Player player = new Player("john doe");
-        String[] starter = new String[] {"1","2","3"};
-        PlinkoGui.main(starter);
-        player.setSpins(PlinkoGui.getSpinsFromPlinko());
+        
+        launch(args);
+        
     }
 }
