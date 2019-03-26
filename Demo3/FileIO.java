dbec3423bd14fee69313ae70703e9cd627123587
mforman000 <47426001+mforman000@users.noreply.github.com>
2019-03-26 13:33:39 -0600

    Update FileIO.java

--- Demo3/FileIO.java
+++ Demo3/FileIO.java
@@ -1,8 +1,15 @@
 import java.io.*;
 
 public class FileIO{
-    private String fileName = "gameData.txt";
-    public String readScore(){
+    private String  fileName = "gameData.txt";
+    
+  /*  private static FileWriter fwriter = new FileWriter("gameData.txt");
+    private static BufferedWriter  bwriter = new BufferedWriter(fwriter);
+    private static FileReader freader = new FileReader("gameData.txt");
+    private static BufferedReader breader = new BufferedReader(freader);
+    */
+
+    public static String readScore(){
         String line = " ";
         try {
             FileReader freader = new FileReader("gameData.txt");
@@ -15,25 +22,45 @@ public class FileIO{
         }
         return line;
     }
-    public void writeScore(int score){
+    public static void writeScore(int score){
         String line1="High Score: "+ score;
         //String line2="Second line";
         try {
             FileWriter fwriter = new FileWriter("gameData.txt");
             BufferedWriter bwriter = new BufferedWriter(fwriter);
+            
             bwriter.write(line1);
+            bwriter.newLine();
             bwriter.close();
         }
         catch (IOException ioe) {
             System.out.println("IO EXCEPTION");
         }
     }
-    public void writeName(String name){
+    public static void writeName(String name){
         String line2="Name: "+ name;
         //String line2="Second line";
         try {
             FileWriter fwriter = new FileWriter("gameData.txt");
             BufferedWriter bwriter = new BufferedWriter(fwriter);
+            
+            bwriter.write(line2);
+            
+            bwriter.close();
+        }
+        catch (IOException ioe) {
+            System.out.println("IO EXCEPTION");
+        }
+    }
+    public static void write(String name, int score){
+        String line1="Name: "+ name;
+        String line2="High Score: "+ score;
+        //String line2="Second line";
+        try {
+            FileWriter fwriter = new FileWriter("gameData.txt");
+            BufferedWriter bwriter = new BufferedWriter(fwriter);
+            bwriter.write(line1);
+            bwriter.newLine();
             bwriter.write(line2);
             bwriter.close();
         }
@@ -42,9 +69,9 @@ public class FileIO{
         }
     }
 
-    public static void main(String args[]){
+    /** public static void main(String args[]){
         FileIO file = new FileIO();
         file.writeHighScore(50);
         System.out.println(file.readHighScore());
-    }
+    }*/
 }
