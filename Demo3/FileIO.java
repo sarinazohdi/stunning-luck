ddcd698e829f84870857287347265637659691a7
fpl890 <47425978+fpl890@users.noreply.github.com>
2019-03-27 12:04:09 -0600

    Add files via upload

--- Demo3/FileIO.java
+++ Demo3/FileIO.java
@@ -8,7 +8,7 @@ public class FileIO{
     private static FileReader freader = new FileReader("gameData.txt");
     private static BufferedReader breader = new BufferedReader(freader);
     */
-
+    /*
     public static String readScore(){
         String line = " ";
         try {
@@ -51,10 +51,11 @@ public class FileIO{
         catch (IOException ioe) {
             System.out.println("IO EXCEPTION");
         }
-    }
-    public static void write(String name, int score){
+    }*/
+    public static void write(String name, int score, int spins){
         String line1="Name: "+ name;
         String line2="High Score: "+ score;
+        String line3 = "Number of Spins: " + spins;
         //String line2="Second line";
         try {
             FileWriter fwriter = new FileWriter("gameData.txt");
@@ -62,16 +63,60 @@ public class FileIO{
             bwriter.write(line1);
             bwriter.newLine();
             bwriter.write(line2);
+            bwriter.newLine();
+            bwriter.write(line3);
             bwriter.close();
         }
         catch (IOException ioe) {
             System.out.println("IO EXCEPTION");
         }
     }
+    public static String readScore(){
+        String line = " ";
+        try {
+            FileReader freader = new FileReader("gameData.txt");
+            BufferedReader breader = new BufferedReader(freader);
+            breader.readLine();
+            line = breader.readLine();
+        
+        }
+        catch(IOException ioe) {
+            System.out.println("Exception : " + ioe);
+        }
+        return line;
+    }
+    public static String readName(){
+        String line = " ";
+        try {
+            FileReader freader = new FileReader("gameData.txt");
+            BufferedReader breader = new BufferedReader(freader);
+            //breader.readLine();
+            line = breader.readLine();    
+        }
+        catch(IOException ioe) {
+            System.out.println("Exception : " + ioe);
+        }
+        return line;
+    }
+    public static String readSpins(){
+        String line = " ";
+        try {
+            FileReader freader = new FileReader("gameData.txt");
+            BufferedReader breader = new BufferedReader(freader);
+            breader.readLine();
+            breader.readLine();
+            line = breader.readLine();
+        
+        }
+        catch(IOException ioe) {
+            System.out.println("Exception : " + ioe);
+        }
+        return line;
+    }
 
-    /** public static void main(String args[]){
-        FileIO file = new FileIO();
-        file.writeHighScore(50);
-        System.out.println(file.readHighScore());
-    }*/
+    public static void main(String args[]){
+        //FileIO file = new FileIO();
+        write("Firoz", 50,20);
+        System.out.println(readName());
+    }
 }
