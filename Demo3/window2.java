614e65acaca8cc9912b7caf865950a884183bdfb
dcomperen <47041666+dcomperen@users.noreply.github.com>
2019-03-27 12:52:40 -0600

    Add files via upload

--- Demo3/window2.java
+++ Demo3/window2.java
@@ -23,10 +23,14 @@ import javafx.scene.control.Button;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
 import java.util.Random;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.io.FileInputStream; 
+import java.io.FileNotFoundException; 
 
 /**
  * The main class for the spinning prize board of the Stunning Luck game
- * @author
+ * @author 
  * - Doug Comperen
  * - Matthew Forman
  * - Sarina Zohdi
@@ -36,15 +40,16 @@ import java.util.Random;
 public class window2 extends Application {
 
     public static final int GRID = 150; // sets a static variable that is used to calculate placement of items
-
+    private int returnprize = 0;
     private  Rectangle highlighter = currentSelection(random()); // variable to call the random highlight
-    //public static void main(String[] args) {
-      //  launch(args);
-    //}
+    private int spins = MainMethod.player1.getSpins();
+    public static void main(String[] args) {
+        launch(args);
+    }
 
 /**
  * Builds the main gameboard and positions each element and label
- * @author
+ * @author 
  * - Doug Comperen
  * - Matthew Forman
  * - Sarina Zohdi
@@ -53,7 +58,7 @@ public class window2 extends Application {
  */
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) throws FileNotFoundException {
         Group root = new Group();
         Scene scene = new Scene(root, 900, 900, Color.BLACK);
         primaryStage.setScene(scene);
@@ -61,7 +66,9 @@ public class window2 extends Application {
         Board board = new Board();
         board.populateBoard();
         ArrayList<Square> randomBoard = board.RandomizeList(board.getList()); // gets list of 20 random objects from the board class
-
+        int counter = 0;
+        ImageView wins = wins(returnprize, randomBoard);
+        
         /*
         each line adds an individual rectangle object to create the 20 boxes around the window.
         */
@@ -85,67 +92,118 @@ public class window2 extends Application {
         root.getChildren().add(prizeBox(2 * GRID, 5 * GRID, GRID, GRID));
         root.getChildren().add(prizeBox(3 * GRID, 5 * GRID, GRID, GRID));
         root.getChildren().add(prizeBox(4 * GRID, 5 * GRID, GRID, GRID));
+    
 
+        //root.getChildren().add(prizeBox(GRID, GRID, 4 *GRID, 4 *GRID));
 
-        root.getChildren().add(prizeBox(GRID, GRID, 4 *GRID, 4 *GRID));
+        
 
-
-
-
-
+        
+        
         primaryStage.show();
-
+        //ImageView wins = wins(returnprize, randomBoard);
+        Label spinsRemaining = new Label("Spins Remaining " + Integer.toString(spins));
+        spinsRemaining.setLayoutX(375);
+        spinsRemaining.setLayoutY(200);
+        spinsRemaining.setTextFill(Color.WHITE);
+        root.getChildren().add(spinsRemaining);
+        Label currentScore = new Label("Current cash and prize value: " + Integer.toString(MainMethod.player1.getScore()));
+        currentScore.setLayoutX(375);
+        currentScore.setLayoutY(250);
+        currentScore.setTextFill(Color.WHITE);
+        root.getChildren().add(currentScore);
         root.getChildren().add(highlighter);
+
 
         Timeline timeline = new Timeline(new KeyFrame(Duration.millis(500),
         new EventHandler<ActionEvent>() {
-
+            
             @Override
             public void handle(ActionEvent event){
-
+                
+                root.getChildren().remove(wins);
                 root.getChildren().remove(highlighter);
                 highlighter = currentSelection(random());
                 root.getChildren().add(highlighter);
-
+               
+                //ImageView wins = wins(random(), randomBoard);
+                
+                //wins.setX(375);
+                //wins.setY(200);
+                //root.getChildren().remove(wins);
+                //wins = wins(returnprize, randomBoard);
+                //root.getChildren().add(wins);
+                    
+                    
+                }
+                
+                
             }
 
-        }
-
+        
+        
         ));
         timeline.setCycleCount(15);
-
+        timeline.setOnFinished(event -> {        
+            MainMethod.player1.addScore(randomBoard.get(returnprize).getValue());
+            currentScore.setText("Current cash and prize value: " + Integer.toString(MainMethod.player1.getScore()));
+        });
         Button spin = new Button("SPIN");
         spin.setLayoutX(300);
         spin.setLayoutY(400);
         spin.setPrefSize(300, 100);
-        spin.setOnAction(click -> timeline.play());
+        spin.setOnAction(click -> {
+            if (spins > 0){
+            timeline.play();
+            
+            //MainMethod.player1.addScore(randomBoard.get(returnprize).getValue());
+            spins -= 1;
+            spinsRemaining.setText("Spins Remaining " + Integer.toString(spins));
+            
+            }
+            else if (spins == 0){
+            
+            }
+        });
         root.getChildren().add(spin);
         //timeline.play();
 
         for(int i = 0; i<20;i++){
-            Label label = new Label(randomBoard.get(i).getTitle());
-            label.setTextFill(Color.WHITE);
-            label.setAlignment(Pos.BASELINE_CENTER);
+            ImageView label = wins(i, randomBoard);
             if (i >=0 && i <= 5){
-                label.setLayoutX((i+0.25)*GRID);
-                label.setLayoutY((0.5*GRID));
+                label.setX(i*GRID);
+                label.setY(0);
             }
             else if (i >= 6 && i <= 9) {
-                label.setLayoutX(0.25*GRID);
-                label.setLayoutY((i - 5 + 0.5) * GRID);
+                label.setX(0);
+                label.setY((i - 5) * GRID);
             }
             else if (i >= 10 && i <= 13) {
-                label.setLayoutX(5.25*GRID);
-                label.setLayoutY((i - 9 + 0.5) * GRID);
+                label.setX(5 *GRID);
+                label.setY((i - 9) * GRID);
             }
             else if (i >= 14 && i < 20) {
-                label.setLayoutX((i - 14 + 0.25) *GRID);
-                label.setLayoutY(5.5 * GRID);
+                label.setX((i - 14) *GRID);
+                label.setY(5 * GRID);
             }
-
+            
             root.getChildren().add(label);
 
         }
+
+    }
+
+
+
+    public ImageView wins(int i, ArrayList<Square> randomBoard) throws FileNotFoundException{
+
+        Image labelprize = new Image(new FileInputStream(randomBoard.get(i).getImage()));
+        ImageView label = new ImageView(labelprize);
+        label.setFitHeight(150); 
+        label.setFitWidth(150);
+        label.setPreserveRatio(true); 
+
+        return label;
 
     }
 
@@ -156,7 +214,7 @@ public class window2 extends Application {
  * @param width the total width of the rectangle
  * @param height the height of the rectangle
  * @return prize, a rectangle object
- * @author
+ * @author 
  * - Doug Comperen
  * - Matthew Forman
  * - Sarina Zohdi
@@ -164,13 +222,20 @@ public class window2 extends Application {
  * - Firoz Lakhani
  */
 
-    public Rectangle prizeBox(int x, int y, int width, int height){
+    public ImageView prizeBox(int x, int y, int width, int height) throws FileNotFoundException{
         Rectangle prize = new Rectangle(x, y, width, height);
         prize.setStrokeType(StrokeType.INSIDE);
         prize.setStroke(Color.web("white", 1));
         prize.setStrokeWidth(2);
+        Image prizePic = new Image(new FileInputStream("testimage.png"));
+        ImageView prizeView = new ImageView(prizePic); 
+        prizeView.setX(x); 
+        prizeView.setY(y);
+        prizeView.setFitHeight(150); 
+        prizeView.setFitWidth(150);
+        prizeView.setPreserveRatio(true); 
 
-        return prize;
+        return prizeView;
 
     }
 
@@ -179,7 +244,7 @@ public class window2 extends Application {
  * prizebox creates a rectangle object to pass to the stage
  * @param choice the index of the object being refenced from the arraylist
  * @return selection, a rectangle object
- * @author
+ * @author 
  * - Doug Comperen
  * - Matthew Forman
  * - Sarina Zohdi
@@ -294,6 +359,7 @@ public class window2 extends Application {
         Rectangle selection = new Rectangle(x, y, GRID, GRID);
         selection.setStrokeType(StrokeType.INSIDE);
         selection.setStroke(Color.web("yellow", 0.75));
+        selection.setFill(Color.TRANSPARENT);
         selection.setStrokeWidth(6);
 
         return selection;
@@ -303,7 +369,7 @@ public class window2 extends Application {
     /**
  * random creates a random number between 0 and 19. It is used to randomly select a prize from the arraylist
  * @return a random int between 0 and 19 inclusive
- * @author
+ * @author 
  * - Doug Comperen
  * - Matthew Forman
  * - Sarina Zohdi
@@ -313,15 +379,17 @@ public class window2 extends Application {
 
     public int random(){
         Random r = new Random();
-        return r.nextInt(20);
+        returnprize = r.nextInt(20);
+        return returnprize;
 
 
     }
 
-
+    
 
 
 
 
 
 }
+
